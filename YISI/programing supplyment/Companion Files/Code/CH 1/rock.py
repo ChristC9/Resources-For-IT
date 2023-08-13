acafbdfcbe1fd1cd009288ccbe60cd854d7ed20c
ChristC9 <nyanhtetaungucsy106@gmail.com>
2023-08-13 20:25:35 +0630

    resources for it

---
+++ YISI/programing supplyment/Companion Files/Code/CH 1/rock.py
@@ -0,0 +1,26 @@
+import random
+computer=["rock","paper","scissors"]
+choice=random.choice(computer)
+print("Hint###",choice)
+#choice = "paper"  # Computer chooses paper.
+print ("Rock-paper-scissors: type in your choice:    ")
+player = input ()
+if player == choice:
+    print ("Game is a tie. Please try again.")
+if player == "rock":
+    if choice == "scissors":
+        print ("Congratulations. You win.")
+    else:
+        print ("Sorry - computer wins.")
+elif player == "paper":
+    if choice == "scissors":
+        print ("Sorry - computer wins.")
+    else:
+        print ("Congratulations. You win.")
+elif player == "scissors":
+    if choice == "rock":
+        print ("Sorry - computer wins.")
+    else:
+        print ("Congratulations. You win.")
+else:
+        print ("Error: Select one of: rock, paper, scissors")
